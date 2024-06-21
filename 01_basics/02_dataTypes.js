509fa44090320a36804768724344bb2ee5d346cd
umair-ansari895 <umairansari895@gmail.com>
2024-06-21 04:47:55 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer versions
+let name = "umair"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 52
+// bigint
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
