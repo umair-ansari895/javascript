aa810772610f35e1b35c608253fd205ed5cb31d8
umair-ansari895 <umairansari895@gmail.com>
2024-06-22 05:51:11 +0000

    conversion of data type

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,37 @@ let someNumber = 35
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ********************** operation ****************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2); // 2 to the power 2
+
+let str1 = "umair"
+let str2 = " ansari"
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log(1 + 2);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log(1 + 2 + 3);
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+console.log(1 + 2 + "3" + 4 + 5);
+console.log(1 + 2 + "3" + 4 + 5 + 6 + 7 + 8);
+console.log(1 + 2 + (3 * 4) + (5 % 6));
+console.log(true);
+console.log(+true);
+// console.log(true+); // error
